6740d815c4965adae822cac58e279673dfa1ac3c
Luis Nunez <18161199@itoaxaca.edu.mx>
2025-10-12 23:11:21 -0600

    Pruebas por cambio de codigo 1.1

--- src/proyecto/proyecto.module.ts
+++ src/proyecto/proyecto.module.ts
@@ -1,14 +1,24 @@
-// src/proyecto/proyecto.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProyectoService } from './proyecto.service';
 import { ProyectoController } from './proyecto.controller';
+import { ProyectoService } from './proyecto.service';
 import { Proyecto } from './proyecto.entity';
-import { ProyectoImagen } from '../proyecto-imagen/proyecto-imagen.entity'; 
+import { Comunidad } from '../comunidad/comunidad.entity';
+import { PersonaProyecto } from '../persona-proyecto/persona-proyecto.entity';
+import { ProyectoImagen } from '../proyecto-imagen/proyecto-imagen.entity';
+import { HistorialFaseModule } from '../historial-fase/historial-fase.module';
+import { HistorialFase } from '../historial-fase/historial-fase.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Proyecto, ProyectoImagen]),
+    TypeOrmModule.forFeature([
+      Proyecto, 
+      Comunidad, 
+      PersonaProyecto, 
+      ProyectoImagen,
+      HistorialFase
+    ]),
+    HistorialFaseModule,
   ],
   controllers: [ProyectoController],
   providers: [ProyectoService],
